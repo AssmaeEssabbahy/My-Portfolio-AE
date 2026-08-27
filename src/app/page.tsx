a499633ace8fe36029e2821951d94df5ce287d7e
assmaaa <assmae.essabbahy2003@gmail.com>
2026-08-27 16:07:07 +0000

    Fix hero navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,9 @@ export default function Home() {
       default:
         return (
           <>
-            <Hero />
+            <Hero
+              onNavigate={(section) => setActiveSection(section)}
+            />
             <TechStack />
           </>
         );
