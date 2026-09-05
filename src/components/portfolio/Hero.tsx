a3c22b1ef7a075498aef3c0ecb77f3299b11015b
assmaaa <assmae.essabbahy2003@gmail.com>
2026-09-05 14:07:03 +0000

    Update CV

--- src/components/portfolio/Hero.tsx
+++ src/components/portfolio/Hero.tsx
@@ -354,7 +354,7 @@ export default function Hero({ onNavigate }: HeroProps) {
 
             {/* DOWNLOAD CV */}
             <a
-              href="/CV_ASSMAE ESSABBAHY_Indus & Logis Engineer.pdf"
+              href="/CV_ASSMAE ESSABBAHY_ING GIL_FR&ANG (2).pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="
